3054df2f528880d508f8c88601be7d71b0ff0cf2
Youssef Abdallah <88114977+YoussefAbdallah987@users.noreply.github.com>
2023-07-05 01:27:01 +0300

    Add files via upload

---
+++ login_page/login.php
@@ -0,0 +1,47 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"]==="POST"){
+
+    // open connection with database
+    require_once("connection.php");
+    
+    $email = $_POST["email"];
+    $password= $_POST["password"];
+
+    //check from email and password to login
+    $query = "SELECT `email`, `password` FROM `data` WHERE `email` = ' {$email} ' AND `password` = ' {$password} ';" ;
+    
+    $result= mysqli_query($dbconnection,$query);
+
+
+    if(mysqli_num_rows($result) > 0){
+        die("login successfully . ");
+        mysqli_close($dbconnection);
+    }else{
+        $error="<h4 style='color: red;'>your email or password is wrong";
+    }
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login Page</title>
+    </head>
+    <body>
+        <h1 style="text-align: center;">Login page</h1>
+        <form style="text-align: center;" action="" method="POST">
+            <?php echo @$error;?>
+            <br>
+            <br>
+            <input type="email" name="email" placeholder="example@gmail.com">
+            <br>
+            <br>
+            <input type="password" name="password" placeholder="password">
+            <br>
+            <br>
+            <input type="submit" name="Login">
+        </form>
+    </body>
+</html>
